2c89d0e6eb1bc17a06f45dd2ee39bea69f341ec7
vkomarichev <trotill@rambler.ru>
2024-12-12 16:40:04 +0300

    Task3_03

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -25,14 +25,12 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println(isLeap(2100));
+        System.out.println(isLeap(2104));
     }
 
     static boolean isLeap(int year) {
 
-        if ((year > 0 || year < 3000) && year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-            return true;
-        }
-        return false;
+        return year > 0 && year < 3000 && year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
+
 }
