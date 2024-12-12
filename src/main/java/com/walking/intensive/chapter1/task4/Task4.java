730bf8fddb9128bb21bc601e22636cf36d64ed4f
vkomarichev <trotill@rambler.ru>
2024-12-12 13:14:14 +0300

    Task4_01

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -24,30 +24,40 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-        double a = 0;
-        double b = -4;
-        double c = 10;
+        double a = 1;
+        double b = -8;
+        double c = 15;
         System.out.println(solveEquation(a, b, c));
     }
 
     static String solveEquation(double a, double b, double c) {
-        if (a == 0 && c == 0 && b == 0) {
-            return "Бесконечное множество решений.";
-        }
-        if (a == 0 && c != 0 && b != 0) {
+
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    return "Бесконечное множество решений.";
+                }
+                return "Количество решений: 0.";
+            }
             return "Количество решений: 1. Корень: " + (-c / b);
         }
-        double deskrim = b * b - 4 * a * c;
 
-        if (deskrim < 0 || (a == 0 && b == 0 )) {
+        double deskriminant = b * b - 4 * a * c;
+
+        if (deskriminant < 0) {
             return "Количество решений: 0.";
         }
-        if (deskrim == 0) {
+
+        if (deskriminant == 0) {
             return "Количество решений: 1. Корень: " + (-b / (2 * a));
         }
-        if ((-b - Math.sqrt(deskrim) / (2 * a)) > (-b + Math.sqrt(deskrim) / (2 * a))){
-            return "Количество решений: 2. Корни: " + ((-b + Math.sqrt(deskrim)) / (2 * a)) + ";" + ((-b - Math.sqrt(deskrim)) / (2 * a));
+
+        double x1 = (-b + Math.sqrt(deskriminant)) / (2 * a);
+        double x2 = (-b - Math.sqrt(deskriminant)) / (2 * a);
+
+        if (x1 > x2) {
+            return "Количество решений: 2. Корни: " + x2 + ";" + x1;
         }
-        return "Количество решений: 2. Корни: " + ((-b - Math.sqrt(deskrim)) / (2 * a)) + ";" + ((-b + Math.sqrt(deskrim)) / (2 * a));
+        return "Количество решений: 2. Корни: " + x1 + ";" + x2;
     }
 }
